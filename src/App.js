00a98ce0ee81a1c365956f7259434ddd187af8ce
William Lee <williamlee@Williams-MacBook-Pro.local>
2021-04-14 20:47:05 -0700

    Simple skeleton and setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Container, Grid } from '@material-ui/core';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Resume from './pages/Resume/Resume';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid container>
+        <Grid item lg={3} md={4} sm={12} xs={12} style={{backgroundColor: 'blue'}}>
+          <Profile />
+        </Grid>
+        <Grid item xs style={{backgroundColor: 'red'}}>
+          <Header />
+          <Router>
+            <Switch>
+              <Route exact path='/'>
+               <Resume />
+              </Route>
+              <Route path='/portfolio'>
+               <Portfolio />
+              </Route>
+            </Switch>
+          </Router>
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
